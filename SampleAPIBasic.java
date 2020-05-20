bbc766dd393e184da7f327553c7c1b32763ef6a9
surendrap86 <59413736+surendrap86@users.noreply.github.com>
2020-05-20 11:17:08 +0530

    Add files via upload

---
+++ SampleAPIBasic.java
@@ -0,0 +1,12 @@
+import static io.restassured.RestAssured.given;
+
+public class SampleAPIBasic {
+
+	
+		public static void main(String[] args) {
+	
+		System.out.println("Hello");
+		given().
+		
+		}
+}
